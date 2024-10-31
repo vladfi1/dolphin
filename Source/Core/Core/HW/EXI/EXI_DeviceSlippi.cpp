0e47b74acb12f04c34b15c74f9c96970b4c0a4f2
Vlad Firoiu <vladfi2@gmail.com>
2024-10-31 16:01:36 +0000

    Briefly sleep when stalling to avoid busy loop.

--- Source/Core/Core/HW/EXI/EXI_DeviceSlippi.cpp
+++ Source/Core/Core/HW/EXI/EXI_DeviceSlippi.cpp
@@ -1307,6 +1307,8 @@ void CEXISlippi::handleOnlineInputs(u8* payload)
   {
     // Send inputs that have not yet been acked
     slippi_netplay->SendSlippiPad(nullptr);
+
+    Common::SleepCurrentThread(6);
   }
   else
   {
