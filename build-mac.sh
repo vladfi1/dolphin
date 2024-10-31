b3a0a549519974682c616ae5fa877f75e2571233
Vlad Firoiu <vladfi2@gmail.com>
2024-10-31 12:12:47 +0000

    Update build-mac.sh for nogui + playback.

--- build-mac.sh
+++ build-mac.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 # build-mac.sh
 
-QT_BREW_PATH=$(brew --prefix qt@6)
-CMAKE_FLAGS="-DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DQT_DIR=${QT_BREW_PATH}/lib/cmake/Qt6"
+BUILD_DIR=./build-nogui
+DATA_SYS_PATH="./Data/Sys"
+
+CMAKE_FLAGS="-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
+CMAKE_FLAGS+=' -DLINUX_LOCAL_DEV=true -DENABLE_HEADLESS=true -DENABLE_QT=false'
 
 # For some reason the system xxhash library doesn't get properly linked,
 # at least on my M1. The clang command gets -lxxhash, but probably needs
@@ -16,6 +19,8 @@ export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/lib:/usr/lib/
 if [ "$1" == "playback" ]
 then
         echo "Using Playback build config"
+        CMAKE_FLAGS+=" -DSLIPPI_PLAYBACK=true"
+        BUILD_DIR+="-playback"
 else
         echo "Using Netplay build config"
         CMAKE_FLAGS+=" -DSLIPPI_PLAYBACK=false"
@@ -27,8 +32,20 @@ then
 fi
 
 # Move into the build directory, run CMake, and compile the project
-mkdir -p build
-pushd build
+mkdir -p $BUILD_DIR
+pushd $BUILD_DIR
 cmake ${CMAKE_FLAGS} ..
-cmake --build . --target dolphin-emu -- -j$(sysctl -n hw.ncpu)
+cmake --build . --target dolphin-nogui -- -j$(sysctl -n hw.ncpu)
 popd
+
+BINARY_PATH="${BUILD_DIR}/Binaries"
+SYS_PATH="${BINARY_PATH}/Sys"
+
+rm -rf ${SYS_PATH}
+cp -r ${DATA_SYS_PATH} ${SYS_PATH}
+
+# Copy Playback codes.
+if [ "$1" == "playback" ]
+then
+        cp Data/Sys/GameSettings/Playback/* ${SYS_PATH}/GameSettings/
+fi
